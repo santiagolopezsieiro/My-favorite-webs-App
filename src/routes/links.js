f2039800d86462f4bccbd2ed62948d2ce84831fb
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-06-12 11:54:56 -0300

    fixed minor bugs

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,37 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../database');
+const {isLoggedIn} = require('../lib/auth')
 
-router.get('/add', (req, res) => {
+router.get('/add',isLoggedIn, (req, res) => {
     res.render('links/add')
 });
 
-router.post('/add', async(req, res) => {
+router.post('/add',isLoggedIn, async(req, res) => {
     const { title, url, description } = req.body;
     const newLink = {
         title,
         url,
-        description
+        description,
+        user_id: req.user.id
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
     req.flash('success', 'link saved successfully');    
     res.redirect('/links');
 });
 
-router.get("/", async(req, res) => {
-    const links = await pool.query('SELECT * FROM links');
+router.get("/",isLoggedIn, async(req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     console.log(links);
     res.render('links/list',{links});
 });
 
- router.get("/delete/:id", async (req, res) => {
+ router.get("/delete/:id",isLoggedIn, async (req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM links WHERE ID = ?',[id]);
     req.flash('success','ink deleted!')
     res.redirect("/links");
  });
 
- router.get("/edit/:id", async (req, res) => { //aca me trae el boton edit y luego redirecciona a links/edit
+ router.get("/edit/:id",isLoggedIn, async (req, res) => { //aca me trae el boton edit y luego redirecciona a links/edit
      const { id } = req.params;
      const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
      console.log(links)
@@ -40,7 +42,7 @@ router.get("/", async(req, res) => {
     res.render('links/edit', {link: links[0]});
  });
 
- router.post("/edit/:id", async (req, res) => {
+ router.post("/edit/:id",isLoggedIn, async (req, res) => {
      const { id } = req.params;//este id es para que lo encuentre en la tabla de la BD
      const { title, description, url } = req.body;//los datos nuevos a actualizar
      const newLink = {
